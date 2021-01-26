047bedc308c01f264d55fb09b9c8bb4bfbc8d9a2
Huy Vuong <huy-vuong@users.noreply.github.com>
2021-01-25 23:07:12 -0800

    Add bootstrap script for macOS

---
+++ macos/bootstrap.sh
@@ -0,0 +1,40 @@
+# Git
+git
+
+# Homebrew - https://brew.sh/
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/${USER}/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+
+# Oh My Zsh - https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# wget - https://www.gnu.org/software/wget/
+brew install wget
+
+# exa - https://the.exa.website/
+brew install exa
+
+# rg - https://github.com/BurntSushi/ripgrep
+brew install ripgrep
+
+# bat - https://github.com/sharkdp/bat
+brew install bat
+
+# fd - https://github.com/sharkdp/fd
+brew install fd
+
+# sd - https://github.com/chmln/sd
+brew install sd
+
+# Node.js - https://nodejs.org/
+brew install node
+
+# OpenJDK -
+brew install openjdk
+
+# Emacs - https://www.gnu.org/software/emacs/
+brew install emacs
+
+# GitHub - https://cli.github.com/
+brew install gh
